c8b8368a19ac267c8bc24effc25f404f31da31cd
k725 <k725@users.noreply.github.com>
2014-09-14 09:02:31 +0900

    Update preview.js

--- public/tyozo-modoki/client/preview.js
+++ public/tyozo-modoki/client/preview.js
@@ -7,40 +7,25 @@
  * for more info visit http://cssglobe.com/post/1695/easiest-tooltip-and-image-preview-using-jquery
  *
  */
-
-this.screenshotPreview = function(){
-	/* CONFIG */
-
-		xOffset = 10;
-		yOffset = 30;
-
-		// these 2 variable determine popup's distance from the cursor
-		// you might want to adjust to get the right result
-
-	/* END CONFIG */
-	$("a.screenshot").hover(function(e){
-		this.t = this.title;
-		this.title = "";
-		var c = (this.t != "") ? "<br/>" + this.t : "";
-		$("body").append("<p id='screenshot'><img src='"+ this.rel +"' alt='url preview' />"+ c +"</p>");
-		$("#screenshot")
-			.css("top",(e.pageY - xOffset) + "px")
-			.css("left",(e.pageX + yOffset) + "px")
-			.fadeIn("fast");
-	},
-	function(){
-		this.title = this.t;
-		$("#screenshot").remove();
-	});
-	$("a.screenshot").mousemove(function(e){
-		$("#screenshot")
-			.css("top",(e.pageY - xOffset) + "px")
-			.css("left",(e.pageX + yOffset) + "px");
-	});
-};
-
-
-// starting the script on page load
 $(document).ready(function(){
-	screenshotPreview();
+	var xOffset = -10,
+		yOffset = 15;
+
+	$(document).on('mouseenter', 'a.screenshot', function(e){
+		$('body').append('<p id="screenshot"><img src="'+ this.rel +'" alt="url preview" /></p>');
+		$('#screenshot')
+			.css('top', (e.pageY - xOffset) + 'px')
+			.css('left', (e.pageX + yOffset) + 'px')
+			.fadeIn();
+	});
+
+	$(document).on('mouseleave', 'a.screenshot', function(){
+		$('#screenshot').remove();
+	});
+
+	$('a.screenshot').mousemove(function(e){
+		$('#screenshot')
+			.css('top', (e.pageY - xOffset) + 'px')
+			.css('left', (e.pageX + yOffset) + 'px');
+	});
 });
